19b2231e4941cc0ae0b2eb30bd070fee087e0aa1
iphyer <shenmingren09@gmail.com>
2020-09-24 20:33:48 -0500

    Conflicting Checking

--- modifyJSON.py
+++ modifyJSON.py
@@ -12,7 +12,12 @@ import random
 with open('forecast.json') as infile:
     data = json.load(infile)
 
+dataTime_set =set()
 for dic in data["values"]:
+    if dic['dateTime'] not in dataTime_set:
+        dataTime_set.add(dic['dateTime'])
+    else:
+        print("Conflicting: {}".format(dic))
     dic["override"] = random.randint(100000, 200000)
 
 # convert into JSON:
